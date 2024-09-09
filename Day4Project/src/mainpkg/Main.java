ca9d4f153331c2a06b90a16f836ba962e1096b8e
pallavi-springpeople <pallavi@springpeople.com>
2024-09-09 10:45:48 +0530

    day 4

---
+++ Day4Project/src/mainpkg/Main.java
@@ -0,0 +1,50 @@
+package mainpkg;
+
+import threadPkg.Worker;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		String str = "string1";
+//		str = "another string";
+//		
+////		String str = new String("string1");
+////		str = new String("another string");
+//		
+//		String str1 = "new string";
+//		str1 = str.concat(" " + str1);
+////		System.out.println(str1);
+//		System.out.println(str1.getClass());
+//		
+//		Thread t1 = new Thread(new Worker());
+//		Thread t2 = new Thread(new Worker());
+		
+		Thread t1 = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+//				new Worker().process();
+				new Worker().stage1();
+				
+			}
+		});
+		
+		
+		Thread t2 = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+//				new Worker().process();
+				new Worker().stage2();
+				
+			}
+		});
+		
+		t1.start();
+		t2.start();
+		
+//		System.out.println("After thread completion");
+		
+	}
+
+}
---
+++ Day4Project/src/threadPkg/Worker.java
@@ -0,0 +1,73 @@
+package threadPkg;
+
+//public class Worker implements Runnable{
+//
+//	@Override
+//	public void run() {
+//		for(int i = 0; i < 10; i++) {
+//			System.out.println("Hello " + i);
+//			
+//			try {
+//				Thread.sleep(1000);
+//			} catch (InterruptedException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//		}
+//		
+//	}
+//
+//}
+
+public class Worker extends Thread {
+	private int count;
+	
+//	public void process() {
+//		for(int i = 0; i < 10; i++) {
+//			System.out.println("Thread name: " + Thread.currentThread().getName());
+//			
+//			try {
+//				Thread.sleep(1000);
+//			} catch (InterruptedException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//			count++;
+//			
+//			System.out.println(count);
+//		}
+//	}
+	
+	public void stage1() {
+		for(int i = 0; i < 10; i++) {
+			System.out.println("Thread name: " + Thread.currentThread().getName());
+			
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			count++;
+			
+			System.out.println(count);
+		}
+	}
+	
+	public void stage2() {
+		for(int i = 0; i < 10; i++) {
+			System.out.println("Thread name: " + Thread.currentThread().getName());
+			
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			count++;
+			
+			System.out.println(count);
+		}
+	}
+	
+}
